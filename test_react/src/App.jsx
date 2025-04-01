5b09bedeec7d6dfcc34fe047e87b74190532bba4
EarnNetiya <https://github.com/EarnNetiya/Network_programming>
2025-04-01 20:59:45 +0700

    ss

--- test_react/src/App.jsx
+++ test_react/src/App.jsx
@@ -4,8 +4,26 @@ function App() {
   
 
   return (
-    <div className='grid grid-cols-[10%_90%]'>
-      <p> snsd  </p>
+    <div className='mx-auto max-w-6xl grid grid-cols-[40%_60%]'>
+      <div>
+        <div className='sticky top-0 grid grid-rows-[35%_45%_20%] h-screen'>
+          <div>
+            <div>Name</div>
+            <div>Title</div>
+            <div>Caption</div>
+            <div>View Resume</div>
+          </div>
+          <div>Nav</div>
+          <div className='flex items-end'>Link</div>
+        </div>
+      
+      </div>
+      <div>
+        <div>About</div>      
+        <div className='mb-96'>Experience</div>
+        <div className='mb-96'>Experience</div>
+        <div className='mb-96'>Experience</div>
+      </div>
     </div>
   )
 }
---
+++ test_react/tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+    content: [
+      './src/**/*.{js,jsx,ts,tsx,html}',
+    ],
+    theme: {
+      extend: {
+        colors: {
+            primaryTitle: "#127369",
+            primaryContent: "#4C5958",
+            primarySubcontent: "#b5cecb",
+            primaryBase: "#fdfdfd",
+            primaryAccent: "#10403B",
+            primaryBg: "#f6f6f6",
+        },
+      },
+    },
+    plugins: [],
+}
